a324a5874ae29ee8a2ad6a70bde189930fe27779
realstealthninja <68815218+realstealthninja@users.noreply.github.com>
2024-08-25 12:16:16 +0530

    fix: use reply

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ lunaBot = LunaBot(YOUTUBE_API_KEY, YOUTUBE_CHANNEL_ID, DEV_USER_ID)
 @lunaBot.check
 async def check_blacklist(ctx):
     if ctx.author.id in lunaBot.blacklisted_users:
-        await ctx.send("You are currently blacklisted and cannot use this command.")
+        await ctx.reply("You are currently blacklisted and cannot use this command.")
         return False
     return True
 
