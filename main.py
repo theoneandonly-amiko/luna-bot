0f8ca2e8faac377301f4e65ec723646d7245cf51
realstealthninja <68815218+realstealthninja@users.noreply.github.com>
2024-08-25 12:52:48 +0530

    fix: use default logger

--- main.py
+++ main.py
@@ -11,11 +11,11 @@ DEV_USER_ID = int(os.getenv('DEV_USER_ID'))
 lunaBot = LunaBot(YOUTUBE_API_KEY, YOUTUBE_CHANNEL_ID, DEV_USER_ID)
  # ================= CHECK BLACKLIST =======================
 @lunaBot.check
-async def check_blacklist(ctx):
+async def check_blacklist(ctx) -> bool:
     if ctx.author.id in lunaBot.blacklisted_users:
         await ctx.reply("You are currently blacklisted and cannot use this command.")
         return False
     return True
 
 if __name__ == "__main__":
-    lunaBot.run(token)
+    lunaBot.run(token, log_handler=None)
