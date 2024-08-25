3686c39e43556a9aa902a35c572acc52f0a75981
theoneandonly-amiko <ando1234gamer@gmail.com>
2024-08-25 14:44:10 +0700

    Update music.py
    Added defines

--- cogs/music.py
+++ cogs/music.py
@@ -2,7 +2,12 @@ import discord
 from discord.ext import commands
 
 # ================ Music Function ======================
-
+queue = []
+IDLE_TIMEOUT = 30
+current_player = None
+volume_level = 0.8  # Default volume level
+last_channel = None # Store the last active channel
+streaming_mode = False
 
 class YTDLSource(discord.PCMVolumeTransformer):
     def __init__(self, source, *, data, volume=0.5):
