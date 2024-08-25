0d1e9ffe627bba54fa5c7eb278c89a79756a8238
theoneandonly-amiko <ando1234gamer@gmail.com>
2024-08-26 00:52:01 +0700

    update help command: use dropdown

--- cogs/help.py
+++ cogs/help.py
@@ -84,7 +84,7 @@ class HelpCommand(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name='help', help='Shows this help message')
+    @commands.command(name='list', help='Shows this help message')
     async def help(self, ctx):
         embed = discord.Embed(title="Help - Select a Category", color=discord.Color.dark_purple())
         embed.description = "Use the dropdown menu to select a category for help."
