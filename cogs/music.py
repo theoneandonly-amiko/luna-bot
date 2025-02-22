5e8c29e9a27f90e961f7ca0bd5072b455df56095
theoneandonly-amiko <ando1234gamer@gmail.com>
2025-02-22 07:10:10 +0700

    nothing much to say....

--- cogs/music.py
+++ cogs/music.py
@@ -21,7 +21,7 @@ YTDL_OPTIONS = {
     'source_address': '0.0.0.0',
     'cookiefile': 'cookies.txt',  # If using cookies
     'http_headers': {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36',
         'Accept-Language': 'en-US,en;q=0.9',
     },
     'postprocessors': [{
